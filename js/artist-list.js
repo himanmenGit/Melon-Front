afbeee6f0c72f1d5fe91a5f9d9f912d4bfde4fe9
himanmen Git <study.himanmen@gmail.com>
2018-03-22 12:47:28 +0900

    cookie.js추가 token저장후 유저 정보 토큰으로 요청해서 자동 로그인 추가

---
+++ js/artist-list.js
@@ -0,0 +1,42 @@
+var pageNum = 1;
+getArtists(pageNum)
+
+$('#btnMoreArtists').click(function() {
+  clickMoreArtist()
+})
+
+function clickMoreArtist() {
+  getArtists(++pageNum);
+}
+
+function getArtists(inner_pageNum) {
+  axios({
+      url: 'http://localhost:8000/api/artist/',
+      method: 'get',
+      params: {
+        page: inner_pageNum
+      }
+    })
+    .then(function(response) {
+      var artists = response.data.results;
+      var artistListElement = $('.artist-list');
+      for (var i = 0; i < artists.length; i++) {
+        var currentArtist = artists[i];
+
+        var curArtistElement = $('#artist-item-template').clone();
+
+        console.log(curArtistElement)
+
+        curArtistElement.find('.artist-name').text(currentArtist.name);
+        curArtistElement.find('.artist-img-profile').attr('src', currentArtist.img_profile)
+
+        artistListElement.append(curArtistElement)
+      }
+      if (!response.data.next) {
+        $('#btnMoreArtists').css("display", "none")
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
